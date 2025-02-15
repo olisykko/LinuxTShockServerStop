95f15b215479301c03472e840dad7799df6b2d11
oli sykko <corejzxs@gmail.com>
2025-02-15 16:52:22 +0200

    Добавьте файлы проекта.

---
+++ LinuxTShockServerStop/Plugin.cs
@@ -0,0 +1,65 @@
+﻿using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace LinuxTShockServerStop
+{
+    [ApiVersion(2, 1)]
+    public class Plugin : TerrariaPlugin
+    {
+        public override string Name => "LinuxTShockServerStop";
+        public override string Author => "oli";
+        public override Version Version => new("1.0");
+
+        public Plugin(Main game) : base(game) { }
+        public override void Initialize()
+        {
+            ServerApi.Hooks.GamePostInitialize.Register(this, OnPostInitialize);
+        }
+
+        private void OnPostInitialize(EventArgs args)
+        {
+            if (OperatingSystem.IsLinux())
+            {
+                static void AddCommand(Command c)
+                {
+                    Commands.ChatCommands.RemoveAll(c2 => c2.Names.Any(n => c.Names.Contains(n)));
+                    Commands.ChatCommands.Add(c);
+                }
+                AddCommand(new Command(Permissions.maintenance, Stop, "stop", "exit", "off"));
+                AddCommand(new Command(Permissions.maintenance, StopNoSave, "stop-nosave", "exit-nosave", "off-nosave"));
+                Console.CancelKeyPress += OnConsoleCancel;
+            }
+        }
+
+        private void OnConsoleCancel(object? sender, ConsoleCancelEventArgs e)
+        {           
+            StopServer(true, "");
+        }
+        public void Stop(CommandArgs e)
+        {
+            if (Main.ServerSideCharacter)
+                foreach (var player in TShock.Players.Where(p => p != null && p.IsLoggedIn && !p.IsDisabledPendingTrashRemoval))
+                    player.SaveServerCharacter();
+            StopServer(true, e.Parameters.Count == 0 ? "Server shutting down!" : "Server shutting down: " + string.Join(" ", e.Parameters));
+        }
+        public void StopNoSave(CommandArgs e)
+        {
+            StopServer(false, e.Parameters.Count == 0 ? "Server shutting down!" : "Server shutting down: " + string.Join(" ", e.Parameters));
+        }
+        public void StopServer(bool saveWorld = true, string reason = "Server shutting down!")
+        {
+            TShock.ShuttingDown = true;
+            if (saveWorld)
+                TShock.Utils.SaveWorld();
+            foreach (var player in TShock.Players.Where(p => p != null))
+            {
+                if (player.IsLoggedIn)
+                    player.SaveServerCharacter();
+                player.Disconnect(reason);
+            }
+            TShock.Utils.Broadcast(reason, 255, 0, 0);
+            Environment.Exit(0);
+        }
+    }
+}
